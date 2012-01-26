3ac67a0ff973b2c1caf5675e05699d8eedf8ec30
greglevilain <greglevilain@hotmail.com>
2012-01-26 07:23:31 +0100

    Module1 boostrap.

---
+++ GitTest-module1/src/test/java/gl/test/git/module1/Test1.java
@@ -0,0 +1,13 @@
+package gl.test.git.module1;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void verify() throws Exception {
+		Assert.assertTrue(true);
+	}
+}
